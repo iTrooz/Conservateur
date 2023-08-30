55ff78e5b8c662225fe12b19a847b91a3962ba99
iTrooz <hey@itrooz.fr>
2023-08-30 14:49:51 +0200

    check messages on startup for missed best memes

--- src/events.ts
+++ src/events.ts
@@ -1,4 +1,4 @@
-import { type Client, Events, TextChannel, Message } from 'discord.js'
+import { type Client, Events, TextChannel, Message, User, MessageReaction, PartialMessageReaction, PartialUser } from 'discord.js'
 import { CONFIG } from './main'
 import { LOGGER } from './logger'
 
@@ -15,7 +15,37 @@ function getMessageLinks(message: Message): string[] {
 
 export default (client: Client): void => {
 
-    client.on(Events.MessageReactionAdd, async (reaction, user) => {
+    client.on(Events.ClientReady, async () => {
+        let logger = LOGGER.getSubLogger({ "name": "init_reaction" });
+        logger.info("Checking reactions from existing messages")
+
+        for (const [guild, guildConfig] of Object.entries(CONFIG.servers)) {
+
+            // find memes channel
+            let memesChannel = await client.channels.fetch(guildConfig.memes_channel)
+            if (!(memesChannel instanceof TextChannel)) {
+                logger.warn(`memes channel of guild ${guild} is not a text channel`)
+                continue;
+            }
+            // loop messages in that channel
+            let messages = await memesChannel.messages.fetch({ limit: 100 })
+            for (let [_, message] of messages) {
+                // find reactions for that message
+                for (let [_, reaction] of message.reactions.cache) {
+                    // check the reaction
+                    if(await handleReaction(reaction)) {
+                        logger.info(`Found and sent best meme that wasn't posted: ${reaction.message.url}`);
+                    }
+                }
+            }
+        }
+
+        logger.info(`Finished !`);
+    })
+
+    client.on(Events.MessageReactionAdd, (reaction, user) => { handleReaction(reaction) });
+
+    async function handleReaction(reaction: MessageReaction | PartialMessageReaction): Promise<boolean> {
         const logger_id = Math.floor(Math.random() * 10000)
         const logger = LOGGER.getSubLogger({ "name": `reaction/${logger_id}` })
 
@@ -24,28 +54,27 @@ export default (client: Client): void => {
         let guildConfig = CONFIG.servers[reaction.message.guild!.id];
         if (!guildConfig) {
             logger.debug("but there is no config for this guild")
-            return;
+            return false;
         };
-
 
         // verify we are in the right channel
         if (guildConfig.memes_channel != reaction.message.channel.id) {
             logger.debug("in the wrong channel");
-            return;
+            return false;
         }
 
         // verify if this is the right reaction
         if ((reaction.emoji.id && reaction.emoji.id == guildConfig.emoji) || (reaction.emoji.name == guildConfig.emoji)) {
         } else {
-            logger.debug("wrong emoji");
-            return;
+            logger.debug(`wrong emoji (${reaction.emoji.id || reaction.emoji.name})`);
+            return false;
         }
 
         // load reaction number, and verify we passed the threshold of reactions
         await reaction.fetch()
         if (!reaction.count || reaction.count < guildConfig.needed_reactions) {
             logger.debug("threshold not passed");
-            return;
+            return false;
         };
 
         // resolve best-memes channel
@@ -57,7 +86,7 @@ export default (client: Client): void => {
         // the 'await' after this comment are being resolved, and the message may be posted twice
         if (sentBestMemes.includes(reaction.message.id)) {
             LOGGER.debug("best-memes channel already had that meme (id cache)")
-            return;
+            return false;
         }
         sentBestMemes.push(reaction.message.id);
         // now that this is done, we can use 'await'
@@ -78,12 +107,13 @@ export default (client: Client): void => {
         let bestMemesLinks = bestMemes.map(msg => getMessageLinks(msg)).flat();
         if (links.every(link => bestMemesLinks.includes(link))) {
             logger.debug("best-memes channel already had that meme (links check)")
-            return;
+            return false;
         }
 
         // send message
         let content = `Meme de ${message.author} (${message.url})\n${links.join(" ")}`;
         await bestMemesChannel.send(content);
-        logger.info("Received reaction, and sent meme to best-memes")
-    })
+        logger.info("Received reaction, and sent meme to best-memes");
+        return true;
+    }
 }
